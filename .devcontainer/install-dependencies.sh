8b0a652d2890d4aa6dce2ea96a61d4bb88c2b080
Crayon <crayon1@rocketmail.com>
2021-09-18 12:32:27 -0400

    Add CI

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -ex
 
-# Install required packages
-dkp-pacman --sync --sysupgrade --refresh --needed --noconfirm wii-dev
+# Install / update required packages
+if [ -z ${GITHUB_WORKSPACE} ]; then
+    dkp-pacman --sync --sysupgrade --refresh --needed --noconfirm wii-dev
+fi
 
 # Install GRRLIB
 wget https://github.com/GRRLIB/GRRLIB/archive/refs/heads/master.tar.gz -O - | tar -xz --verbose --directory=/tmp/
 (cd /tmp/GRRLIB-*/GRRLIB && make && make install)
-rm -rf GRRLIB-*
+rm -rf /tmp/GRRLIB-*
 
 # Install fmt library
 wget https://github.com/fmtlib/fmt/archive/refs/tags/8.0.1.tar.gz -O - | tar -xz --verbose --directory=/tmp/
@@ -16,4 +18,4 @@ mkdir build
 cd build
 /opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DFMT_TEST=0 -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc ..
 make install
-rm -rf fmt-*
+rm -rf /tmp/fmt-*
