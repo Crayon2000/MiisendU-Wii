8ccb5211fa50e1eca563a3abb51252e4744ade1b
Crayon <crayon1@rocketmail.com>
2024-10-20 00:30:19 -0400

    Simplify installation

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -14,27 +14,21 @@ rm -rf /tmp/GRRLIB-*
 # Install fmt library
 wget https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz -O - | tar -xz --verbose --directory=/tmp/
 cd /tmp/fmt*
-mkdir build
-cd build
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DFMT_TEST=0 -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc ..
-cmake --build . --target install
+/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DFMT_TEST=0 -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc -B build
+cmake --build build --target install
 rm -rf /tmp/fmt-*
 
 # Install IniPP library
 wget https://github.com/mcmtroffaes/inipp/archive/refs/tags/1.0.12.tar.gz -O - | tar -xz --verbose --directory=/tmp/
 cd /tmp/inipp-*
-mkdir build
-cd build
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc ..
-cmake --build . --target install
+/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc -B build
+cmake --build build --target install
 rm -rf /tmp/inipp-*
 
 # Install RapidJSON
 wget https://github.com/Tencent/rapidjson/archive/refs/heads/master.tar.gz -O - | tar -xz --verbose --directory=/tmp/
 cd /tmp/rapidjson-*
-mkdir build
-cd build
 /opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc -DCMAKE_BUILD_TYPE=Release \
-    -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_CXX11=OFF -DRAPIDJSON_BUILD_CXX17=ON ..
-cmake --build . --target install
+    -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_CXX11=OFF -DRAPIDJSON_BUILD_CXX17=ON -B build
+cmake --build build --target install
 rm -rf /tmp/rapidjson-*
