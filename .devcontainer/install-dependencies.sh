bd2cf00a20d60896149ac33242cae68ac1c43c94
Crayon <crayon1@rocketmail.com>
2024-11-18 00:57:21 -0500

    Use a more recent version of IniPP

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -11,20 +11,6 @@ wget https://github.com/GRRLIB/GRRLIB/archive/refs/heads/master.tar.gz -O - | ta
 (cd /tmp/GRRLIB-*/GRRLIB && make && make install)
 rm -rf /tmp/GRRLIB-*
 
-# Install fmt library
-wget https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz -O - | tar -xz --verbose --directory=/tmp/
-cd /tmp/fmt*
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DFMT_TEST=0 -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc -B build
-cmake --build build --target install
-rm -rf /tmp/fmt-*
-
-# Install IniPP library
-wget https://github.com/mcmtroffaes/inipp/archive/refs/tags/1.0.12.tar.gz -O - | tar -xz --verbose --directory=/tmp/
-cd /tmp/inipp-*
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/portlibs/ppc -B build
-cmake --build build --target install
-rm -rf /tmp/inipp-*
-
 # Install RapidJSON
 wget https://github.com/Tencent/rapidjson/archive/refs/heads/master.tar.gz -O - | tar -xz --verbose --directory=/tmp/
 cd /tmp/rapidjson-*
