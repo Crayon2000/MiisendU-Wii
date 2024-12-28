26f0003b2e8bf8f74fc5f36dc15941ecd67a4f33
Crayon <crayon1@rocketmail.com>
2024-12-28 15:02:46 -0500

    Install grrlib in CMakeLists.txt

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -5,8 +5,3 @@ set -ex
 if [ -z ${GITHUB_WORKSPACE} ]; then
     dkp-pacman --sync --sysupgrade --refresh --needed --noconfirm wii-dev ppc-portlibs
 fi
-
-# Install GRRLIB
-wget https://github.com/GRRLIB/GRRLIB/archive/refs/heads/master.tar.gz -O - | tar -xz --verbose --directory=/tmp/
-(cd /tmp/GRRLIB-*/GRRLIB && make && make install)
-rm -rf /tmp/GRRLIB-*
